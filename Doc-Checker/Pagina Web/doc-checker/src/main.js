88aad727e33fb8f904b562bb0a7f4223b71d6b0d
Alan Alexis Velasco Martinez <56143855+AlanAlexisVM@users.noreply.github.com>
2021-11-23 21:32:23 -0600

    Create login

--- Doc-Checker/Pagina Web/doc-checker/src/main.js
+++ Doc-Checker/Pagina Web/doc-checker/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import Vue from 'vue'   // in Vue 2
+import VueAxios from 'vue-axios'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.use(VueAxios, Axios)
+
 
 new Vue({
   router,
