57a86b8edd8f7752c49f5bbc6cf078a87e673954
JAlejandro99 <72848936+JAlejandro99@users.noreply.github.com>
2022-02-22 09:21:22 -0600

    22/02/2022

--- Doc-Checker/Pagina Web/servidor-node/app.js
+++ Doc-Checker/Pagina Web/servidor-node/app.js
@@ -1,15 +1,35 @@
+// 1 - Invocamos a Express
 const express = require('express');
 const app = express();
 
+//2 - Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
 app.use(express.urlencoded({extended:false}));
 app.use(express.json);
-    
+
+//3- Invocamos a dotenv
 const dotenv = require('dotenv');
+/*
+//6 -Invocamos a bcrypt
+const bcryptjs = require('bcryptjs');
+
+//7- variables de session
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));*/
 
 app.get('/', (req, res)=>{
     res.send('HOLA MUNDO');
 })
 
+//11 - Metodo para la autenticacion
+app.post('/auth', async (req, res)=> {
+    console.log(req);
+});
+
+
 app.listen(3000, (req, res)=>{
-    console.log('SERVER RUNNING IN ...');
+    console.log('SERVER RUNNING IN http://localhost:3000');
 })
