ddcee80500de7d83cc323981efb33cc9d75d7235
Alan Alexis Velasco Martinez <56143855+AlanAlexisVM@users.noreply.github.com>
2022-05-05 20:48:25 -0500

    Update Signos

--- Doc-Checker/Pagina Web/servidor-node/solicitarSignosVitales.js
+++ Doc-Checker/Pagina Web/servidor-node/solicitarSignosVitales.js
@@ -1,14 +1,14 @@
 var fork = require("child_process").fork;
 
-process.send({msj: "Hijo cargado"})
+process.send({ msj: "Hijo cargado" });
 
-process.on('message', msj => {
-    //console.log(msj)
-    for(const placa in msj.placas){
-        var sp1 = fork("signosVitales");
-        sp1.send({placa:msj.placas[placa]});
-        sp1.on('message', msj => {
-            process.send({msj: msj.msj});
-        });
-    }
+process.on("message", (msj) => {
+  //console.log(msj)
+  for (const placa in msj.placas) {
+    var sp1 = fork("signosVitales");
+    sp1.send({ placa: msj.placas[placa] });
+    sp1.on("message", (msj) => {
+      process.send({ msj: msj.msj });
+    });
+  }
 });
